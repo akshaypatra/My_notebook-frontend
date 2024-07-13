30dfa720843c8f85bfe94947e8e2b2ca0911a8c6
Akshay Patra <akshay.patra114@gmail.com>
2024-07-13 10:48:34 +0530

    added about.js part

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,8 +6,8 @@ const About = () => {
   
 
   return (
-    <div>
-      This is about 
+    <div className='about'>
+      MyNotebook is Full stack web application made using MERN stack. <br></br>Its a CRUD application where anyone can login with valid credentials and read,add,delete and update their notes .
     </div>
   )
 }
