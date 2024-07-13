cfa9c022e7f29f98517eeff7ae6c7b96989991ec
Akshay Patra <akshay.patra114@gmail.com>
2024-07-13 10:53:21 +0530

    changed about.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,6 +8,12 @@ const About = () => {
   return (
     <div className='about'>
       MyNotebook is Full stack web application made using MERN stack. <br></br>Its a CRUD application where anyone can login with valid credentials and read,add,delete and update their notes .
+<br></br>
+In this application
+
+Frontend is made using React framework and Bootstrap
+Server is made using express framework
+MongoDB is used as database
     </div>
   )
 }
